3ee1af16401aa8ff4b56a4c83616a041568aa4c5
remy <remy.snidaro@gmail.com>
2019-01-12 16:15:24 +0100

    SearchGame work now ,but no optimised ,WIP

--- src/main/java/com/resnidar/SearchGameDefence.java
+++ src/main/java/com/resnidar/SearchGameDefence.java
@@ -8,35 +8,47 @@ public class SearchGameDefence extends SearchGame {
     static Logger logger = Logger.getLogger(SearchGameDefence.class);
 
     int life;
+
     public SearchGameDefence(Config config) {
         super(config);
         life = config.getLife();
     }
+
     Scanner sc = new Scanner(System.in);
 
-    boolean logic(){
+    boolean logic() {
         char[] userNumberChar;
         String userNumber;
+        String userProp;
         boolean restart = false;
         logger.debug("searchGame en mode defensif actif");
         System.out.println("mode logique actif");
-
-        //futur method userRequest
         System.out.println("veuillez entree le nombre");
-        userNumber = sc.next();
-        userNumberChar = userNumber.toCharArray(); //mise du string dans un tableau de char
-        for (int i = 0; userNumberChar.length > i; i++){
-            System.out.println("chiffre " + i +  " : " + userNumberChar[i] );
-        }
+        userNumberChar = userRequest();
+
 
         //futur method iaTurn
         // TODO: 17/12/2018 demander a l ia de trouver le nombre secret
 // TODO: 2019-01-08  l'ia receptionne le numero
 // TODO: 2019-01-08 l ia regarde si c est au milieu
-        char[] iaTab = new char[userNumberChar.length];
-        for (int i = 0; i <= life ; i++){ // cette boucle est limité par la vie
-            for (int j = 0; userNumberChar.length < j; j++) { // cette boucle permet de faire toute les cases du tab
 
+        char[] iaTab = new char[userNumberChar.length];
+        char[] userPropTab = new char[userNumberChar.length];
+        for (int i = 0; i < userNumberChar.length; i++) {
+            iaTab[i] = '5';
+        }
+        for (int i = 0; i <= life; i++) { // cette boucle est limité par la vie
+            System.out.print("IA : je te propose ");
+            for (int j = 0; j < iaTab.length; j++)
+                System.out.print(iaTab[j]);
+            System.out.println(" veuillez saisir + , - ou =");
+            userProp = sc.next();
+            userPropTab = userProp.toCharArray();
+            for (int j = 0; userNumberChar.length > j; j++) { // cette boucle permet de faire toute les cases du tab
+                if (userPropTab[j] == '+') {
+                    iaTab[j] += 1;
+                } else if (userPropTab[j] == '-')
+                    iaTab[j] -= 1;
             }
         }
 // TODO: 2019-01-08 l ia regarde si c est 1/4
@@ -49,4 +61,17 @@ public class SearchGameDefence extends SearchGame {
 // TODO: 2019-01-08 doc !!
         return restart;
     }
+
+    private char[] userRequest() {
+        String userNumber;
+        char[] userNumberChar;
+        userNumber = sc.next();
+        userNumberChar = userNumber.toCharArray(); //mise du string dans un tableau de char
+        for (int i = 0; userNumberChar.length > i; i++) {
+            System.out.println("chiffre " + i + " : " + userNumberChar[i]);
+        }
+        return userNumberChar;
+    }
 }
+
+//dichotomie
